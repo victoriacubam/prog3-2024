ee1975aaf4a7ca608a4e3c2a50e7505778b183e0
Victoria Cuba Martinez <victoriacuba01@gmail.com>
2024-04-15 22:34:45 -0300

    Agrega ejercicio 1 - Algoritmo recursivo para determinar si un arreglo está ordenado

--- TP2/Main.java
+++ TP2/Main.java
@@ -2,6 +2,6 @@ package TP2;
 
 public class Main {
     public static void main (String[]args){
-        
+
     }
 }
---
+++ TP2/Recursion.java
@@ -0,0 +1,20 @@
+package TP2;
+
+public class Recursion {
+    //Ejercicio 1
+    //Implemente un algoritmo recursivo que determine si un arreglo de tamaño N está ordenado
+
+    public boolean estaOrdenado (int[] arr, int pos, int value){
+        // Si ya llego al final del arreglo retorna true pq recorrio todo el arreglo y estaba ordenado
+        if (pos > arr.length-1)
+            return true;
+        // Si todavia no llegué al final del arreglo y el valor anterior es menor o igual al valor actual sigue preguntando
+        else if (pos<= arr.length-1 && value <= arr[pos])
+            return estaOrdenado(arr,pos+1 ,arr[pos]);
+        // No esta ordenado
+        else
+            return false;
+    }
+
+
+}
