bf2e6edcf55339c36e971a107a35f5fb965d73e5
Victoria Cuba Martinez <victoriacuba01@gmail.com>
2024-04-15 22:45:59 -0300

    Agrega algoritmo recursivo para determinar si un arreglo esta ordenado ASC y DESC

--- TP2/Main.java
+++ TP2/Main.java
@@ -1,13 +1,15 @@
 package TP2;
-
 import java.util.Arrays;
-
-import static TP2.Recursion.estaOrdenado;
+import static TP2.Recursion.estaOrdenadoAsc;
+import static TP2.Recursion.estaOrdenadoDesc;
 
 public class Main {
     public static void main (String[]args){
-        int [] arr = {1,1,3,4,6};
-        System.out.println("Esta ordenado: " + Arrays.toString(arr) + " - "
-                + estaOrdenado(arr, 0, arr[0]));
+        int [] arr1 = {1,1,3,4,6};
+        System.out.println("Esta ordenado ASCENDENtE: " + Arrays.toString(arr1) + " - "
+                + estaOrdenadoAsc(arr1, 0, arr1[0]));
+        int [] arr2 = {10,9,5,4,1};
+        System.out.println("Esta ordenado DESCENDENTE: " + Arrays.toString(arr2) + " - "
+                + estaOrdenadoDesc(arr2, 0, arr2[0]));
     }
 }
--- TP2/Recursion.java
+++ TP2/Recursion.java
@@ -4,17 +4,31 @@ public class Recursion {
     //Ejercicio 1
     //Implemente un algoritmo recursivo que determine si un arreglo de tamaño N está ordenado
 
-    public static boolean estaOrdenado (int[] arr, int pos, int value){
+    public static boolean estaOrdenadoAsc (int[] arr, int pos, int value){
         // Si ya llego al final del arreglo retorna true pq recorrio todo el arreglo y estaba ordenado
         if (pos > arr.length-1)
             return true;
         // Si todavia no llegué al final del arreglo y el valor anterior es menor o igual al valor actual sigue preguntando
         else if (pos<= arr.length-1 && value <= arr[pos])
-            return estaOrdenado(arr,pos+1 ,arr[pos]);
+            return estaOrdenadoAsc(arr,pos+1 ,arr[pos]);
         // No esta ordenado
         else
             return false;
     }
+
+    public static boolean estaOrdenadoDesc (int[] arr, int pos, int value){
+        // Si ya llego al final del arreglo retorna true pq recorrio todo el arreglo y estaba ordenado
+        if (pos > arr.length-1)
+            return true;
+            // Si todavia no llegué al final del arreglo y el valor anterior es mayor o igual al valor actual sigue preguntando
+        else if (pos<= arr.length-1 && value >= arr[pos])
+            return estaOrdenadoDesc(arr,pos+1 ,arr[pos]);
+            // No esta ordenado
+        else
+            return false;
+    }
+
+    
 
 
 }
